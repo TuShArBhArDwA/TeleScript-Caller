88d7ca96c31833cbda2ccb948ced92261ee98049
Tushar Bhardwaj <116137083+TuShArBhArDwA@users.noreply.github.com>
2024-03-19 11:39:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_API = '7163892742:AAEmex6lIz_ka_3e-muYdZ6zwZ82Gn52GgY'
+BOT_API = '6761737569:AAHvKsm-GVVlGyybypaNCukUkBr__iCpomI'
 OPENAI_KEY = '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'
 my_id = ''
